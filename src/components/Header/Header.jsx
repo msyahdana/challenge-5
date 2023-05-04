f1a86255629f96735b81d01297b36b519a9a5395
msyahdana <msyahdanabdillah@gmail.com>
2023-05-04 16:03:04 +0700

    feature: fix Login.jsx, Register.jsx, ToastifyContainer app.js, NavbarComponent.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,6 @@ import axios from "axios";
 import "../../style/Navbar.css";
 import NavbarComponent from "./NavbarComponent";
 
-const baseUrl = process.env.REACT_APP_BASEURL;
-const apiKey = process.env.REACT_APP_APIKEY;
-
 function Header() {
   const [detailMovie, setDetailMovie] = useState({});
   const [detailMovie2, setDetailMovie2] = useState({});
@@ -15,15 +12,9 @@ function Header() {
   useEffect(() => {
     async function getDetailMovie() {
       try {
-        const response = await axios.get(
-          `${baseUrl}/movie/373571?api_key=${apiKey}&language=en-US`
-        );
-        const response2 = await axios.get(
-          `${baseUrl}/movie/76600?api_key=${apiKey}&language=en-US`
-        );
-        const response3 = await axios.get(
-          `${baseUrl}/movie/505642?api_key=${apiKey}&language=en-US`
-        );
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/373571?api_key=3372fc4e380b3c807cedb51e891bbc9d&language=en-US`);
+        const response2 = await axios.get(`https://api.themoviedb.org/3/movie/76600?api_key=3372fc4e380b3c807cedb51e891bbc9d&language=en-US`);
+        const response3 = await axios.get(`https://api.themoviedb.org/3/movie/505642?api_key=3372fc4e380b3c807cedb51e891bbc9d&language=en-US`);
         setDetailMovie(response.data);
         setDetailMovie2(response2.data);
         setDetailMovie3(response3.data);
@@ -40,11 +31,7 @@ function Header() {
       <NavbarComponent />
       <Carousel>
         <Carousel.Item>
-          <img
-            className="Carousel-img d-block w-100 h-100"
-            src={`https://image.tmdb.org/t/p/original${detailMovie?.backdrop_path}`}
-            alt="First slide"
-          />
+          <img className="Carousel-img d-block w-100 h-100" src={`https://image.tmdb.org/t/p/original${detailMovie?.backdrop_path}`} alt="First slide" />
           <Carousel.Caption className="Movie-caption">
             <h2 className="Movie-caption-title">{detailMovie?.title}</h2>
             <p className="Movie-caption-text">{detailMovie?.overview}</p>
@@ -54,11 +41,7 @@ function Header() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="Carousel-img d-block w-100 h-100"
-            src={`https://image.tmdb.org/t/p/original${detailMovie2?.backdrop_path}`}
-            alt="First slide"
-          />
+          <img className="Carousel-img d-block w-100 h-100" src={`https://image.tmdb.org/t/p/original${detailMovie2?.backdrop_path}`} alt="First slide" />
           <Carousel.Caption className="Movie-caption">
             <h2 className="Movie-caption-title">{detailMovie2?.title}</h2>
             <p className="Movie-caption-text">{detailMovie2?.overview}</p>
@@ -68,11 +51,7 @@ function Header() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="Carousel-img d-block w-100 h-100"
-            src={`https://image.tmdb.org/t/p/original${detailMovie3?.backdrop_path}`}
-            alt="First slide"
-          />
+          <img className="Carousel-img d-block w-100 h-100" src={`https://image.tmdb.org/t/p/original${detailMovie3?.backdrop_path}`} alt="First slide" />
           <Carousel.Caption className="Movie-caption">
             <h2 className="Movie-caption-title">{detailMovie3?.title}</h2>
             <p className="Movie-caption-text">{detailMovie3?.overview}</p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import NavbarComponent from "../components/Header/NavbarComponent";
-import { Form, Button, Container, Row, Col, Card } from "react-bootstrap";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 import { toast } from "react-toastify";
 import Google from "../components/Auth/Google";
@@ -52,55 +52,39 @@ function Register() {
       <div className="vh-100">
         <NavbarComponent />
         <Container className="p-5 mt-5">
-          <h1 className="text-center text-white mb-4 mt-4">
-            Register Your Account
-          </h1>
-          <Card>
-            <Card.Body>
-              <Form onSubmit={onSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label className="text-light">Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </Form.Group>
+          <h1 className="text-center text-white mb-4 mt-4">Register Your Account</h1>
+          <Form onSubmit={onSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="text-light">Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)} />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label className="text-light">Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label className="text-light">Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Group>
-              </Form>
-              <Row>
-                <Col className="text-center ">
-                  <Button className="w-25" variant="danger" type="submit">
-                    Submit
-                  </Button>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="text-center pt-1">
-                  <Google buttonText="Login with Google 🚀" />
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="text-light">Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className="text-light">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+            <Row>
+              <Col className="text-center pt-1">
+                <Button className="w-25" variant="danger" type="submit">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <h3 className="text-light py-2">Or</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <Google buttonText="Register with Google 🚀" />
+              </Col>
+            </Row>
+          </Form>
         </Container>
       </div>
     </>
